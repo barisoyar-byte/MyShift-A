69b2cfc29b595f68c103a9dfa8d15e736c1cc1c6
Barış Oyar <barisoyar@gmail.com>
2026-02-21 21:44:02 +0300

    Initial Commit

---
+++ MyShift A/MyShift_AApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyShift_AApp.swift
+//  MyShift A
+//
+//  Created by Barış Oyar on 21.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct MyShift_AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MyShift ATests/MyShift_ATests.swift
@@ -0,0 +1,17 @@
+//
+//  MyShift_ATests.swift
+//  MyShift ATests
+//
+//  Created by Barış Oyar on 21.02.2026.
+//
+
+import Testing
+@testable import MyShift_A
+
+struct MyShift_ATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
